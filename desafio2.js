5031165a856c1663d707eef56cbef1a70c4c9125
Bruno Bastos Duarte <bastosduartebruno@gmail.com>
2022-04-28 00:33:16 -0300

    Update desafio2.js

--- desafio2.js
+++ desafio2.js
@@ -1,23 +1,3 @@
-/**
- * Um professor de programação, frustrado com a falta de disciplina de seus alunos, decidi cancelar a aula se menos de x alunos estiverem presentes quando a aula for iniciada. O tempo de chegada varia entre:
-
-Normal: tempoChegada <= 0
-Atraso: tempoChegada > 0
-Construa um algoritmo que dado o tempo de chegada de cada aluno e o limite x de alunos presentes, determina se a classe vai ser cancelada ou não ("Aula cancelada” ou “Aula normal”).
-
-Exemplo:
-
-Entrada de dados:
-x = 3
-tempoChegada = [-2, -1, 0, 1, 2]
-
-Saída de dados:
-Aula normal.
-
-Explicação:
-Os três primeiros alunos chegaram no horário. Os dois últimos estavam atrasados. O limite é de três alunos, portanto a aula não será cancelada.
- */
-
 function main() {
   let x = 3;
   let tempoChegada = [-2, -1, 0, 1, 2];
