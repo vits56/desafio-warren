8ff15fac96c48a6e20b5aaa0fbcaebd4a905818f
Bruno Bastos Duarte <bastosduartebruno@gmail.com>
2022-04-28 00:32:35 -0300

    Update desafio1.js

--- desafio1.js
+++ desafio1.js
@@ -1,12 +1,3 @@
-/*
-
-Alguns números inteiros positivos n possuem uma propriedade na qual a soma de n + reverso(n) resultam em números ímpares. Por exemplo, 36 + 63 = 99 e 409 + 904 = 1313. Considere que n ou reverso(n) não podem começar com 0.
-
-Existem 120 números reversíveis abaixo de 1000.
-
-Construa um algoritmo que mostre na tela todos os números n onde a soma de n + reverso(n) resultem em números ímpares que estão abaixo de 1 milhão.
-*/
-
 function main() {
   let resultado = [];
 
